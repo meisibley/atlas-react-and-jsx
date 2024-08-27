b4a4d8b10f65e55674f723d3e5b9aad7a64b6c7d
meisibley <mkong99@yahoo.com>
2024-08-26 23:28:03 -0500

    task0 - render the App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
 import "./index.css";
+import App from "./App";
+import ReactDOM from "react-dom/client";
 
 // Render the App component to the root element
+const div = document.getElementById("root");
+const root = ReactDOM.createRoot(div);
+root.render(<App />);
